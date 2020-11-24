2b598926d9caf731e9c76144318478aeb1529129
Ahmad Nurjati <anurjati16@gmail.com>
2020-11-24 15:09:03 +0700

    Add minor changes

--- app/Controllers/Kalender.php
+++ app/Controllers/Kalender.php
@@ -2,8 +2,16 @@
 
 namespace App\Controllers;
 
+use \App\Models\KalenderModel;
+
 class Kalender extends BaseController
 {
+    protected $KalenderModel;
+    public function __construct()
+    {
+        $this->KalenderModel = new KalenderModel();
+    }
+
     public function index()
     {
         $data = [
